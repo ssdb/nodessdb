264c22b15b6ed8d468cdef48098b4beba12d9e09
aleks_raiden <aleks.raiden@gmail.com>
2019-03-13 19:24:36 +0200

    All hash commands done!

--- SSDB.js
+++ SSDB.js
@@ -25,7 +25,8 @@ exports.connect = function(host, port, timeout, listener){
 	listener = listener || function(){};
 
 	var sock = new net.Socket();
-	sock.on('error', function(e){
+	sock.on('error', function(e){		
+		console.log('333');
 		if(!connected){
 			listener('connect_failed', e);
 		}else{
@@ -33,13 +34,26 @@ exports.connect = function(host, port, timeout, listener){
 			callback(['error']);
 		}
 	});
-	sock.connect(port, host, function(){
-		console.log('Socket connected!');
-		
+	
+	sock.on('data', function(data){
+		recv_buf = build_buffer([recv_buf, data]);
+		while(recv_buf.length > 0){
+			var resp = parse();
+			if(!resp){
+				break;
+			}
+			resp[0] = resp[0].toString();
+			var callback = callbacks.shift();
+			callback(resp);
+		}
+	});
+
+	sock.connect({port:8888, host:'localhost'}, function(){
+		//console.log('Socket connected!');		
 		connected = true;
 		sock.setNoDelay(true);
 		sock.setKeepAlive(true);
-		sock.setTimeout(timeout);
+		sock.setTimeout(0); //timeout);
 		listener(0, self);
 	});
 
@@ -91,24 +105,13 @@ exports.connect = function(host, port, timeout, listener){
 			bs.push('\n');
 		}
 		bs.push('\n');
+		
 		var req = build_buffer(bs);
+		
 		sock.write(req);
 		//console.log('write ' + req.length + ' bytes');
 		//console.log('write: ' + req);
 	}
-
-	sock.on('data', function(data){
-		recv_buf = build_buffer([recv_buf, data]);
-		while(recv_buf.length > 0){
-			var resp = parse();
-			if(!resp){
-				break;
-			}
-			resp[0] = resp[0].toString();
-			var callback = callbacks.shift();
-			callback(resp);
-		}
-	});
 
 	function memchr(buf, ch, start){
 		start = start || 0;
@@ -432,13 +435,17 @@ exports.connect = function(host, port, timeout, listener){
 
 	// callback(err, [])
 	self.hlist = function(name_start, name_end, limit, callback){
+		if (typeof(limit) == 'function'){
+			callback = limit;
+			limit = 9223372036854775807; //max elements at hash
+		}
 		self.request('hlist', [name_start, name_end, limit], function(resp){
 			if(callback){
 				var err = resp[0] == 'ok'? 0 : resp[0];
 				var data = [];
 				for(var i=1; i<resp.length; i++){
-					var k = resp[i].toString();
-					data.push(k);
+
+					data.push( resp[i].toString() );
 				}
 				callback(err, data);
 			}
@@ -461,11 +468,193 @@ exports.connect = function(host, port, timeout, listener){
 		});
 	}
 	
+	//hclear name Delete all keys in a hashmap.
 	self.hclear = function(key, callback){
 		self.request('hclear', [key], function(resp){
 			if(callback){
 				var err = resp[0] == 'ok'? 0 : resp[0];
 				callback(err);
+			}
+		});
+	}
+	
+	//hgetall name Returns the whole hash, as an array of strings indexed by strings.	
+	self.hgetall = function(key, callback){
+		self.request('hgetall', [key], function(resp){
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				var data = {};
+				for(var i=1; i<resp.length; i+=2){
+					data[ resp[i].toString() ] = resp[i+1].toString();
+				}
+				callback(err, data);
+			}
+		});
+	}
+	
+	//hexists name key Verify if the specified key exists in a hashmap.
+	self.hexists = function(name, key, callback){
+		self.request('hexists', [name, key], function(resp){
+			if(callback){
+				let err = resp[0] == 'ok'? 0 : resp[0];
+				let res = parseInt( resp[1].toString() );
+				
+				if (res === 0) res = false; else res = true;
+				
+				callback(err, res);
+			}
+		});
+	}
+	
+	//hincr name key [num] Increment the number stored at key in a hashmap by num
+	self.hincr = function(name, key, num, callback){
+		if (typeof(num) == 'function'){
+			callback = num;
+			num = 1;
+		}
+					
+		self.request('hincr', [name, key, num], function(resp){
+			if(callback){
+				let err = resp[0] == 'ok'? 0 : resp[0];
+		
+				callback(err, parseInt( resp[1].toString() ));
+			}
+		});
+	}
+	
+	//hrlist name_start name_end limit List hashmap names in range (name_start, name_end].
+	self.hrlist = function(name_start, name_end, limit, callback){
+		if (typeof(limit) == 'function'){
+			callback = limit;
+			limit = 9223372036854775807; //max elements at hash
+		}
+		
+		self.request('hrlist', [name_start, name_end, limit], function(resp){
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				var data = [];
+				for(var i=1; i<resp.length; i++){
+					data.push( resp[i].toString() );
+				}
+				callback(err, data);
+			}
+		});
+	}
+	
+	//hkeys name key_start key_end List keys of a hashmap in range (key_start, key_end].
+	self.hkeys = function(name, key_start, key_end, limit, callback){
+		if (typeof(limit) == 'function'){
+			callback = limit;
+			limit = 9223372036854775807; //max elements at hash
+		}		
+		
+		self.request('hkeys', [name, key_start, key_end, limit], function(resp){			
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				var data = [];
+				for(var i=1; i<resp.length; i++){
+					data.push( resp[i].toString() );
+				}
+				callback(err, data);
+			}
+		});
+	}
+	
+	//hrscan name key_start key_end limit List key-value pairs with keys in range (key_start, key_end], in reverse order.
+	self.hrscan = function(name, key_start, key_end, limit, callback){
+		if (typeof(limit) == 'function'){
+			callback = limit;
+			limit = 9223372036854775807; //max elements at hash
+		}
+		
+		self.request('hrscan', [name, key_start, key_end, limit], function(resp){
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				if(resp.length % 2 != 1){
+					callback('error');
+				}else{
+					var data = {};
+					for(var i=1; i<resp.length; i+=2){
+						data[ resp[i].toString() ] = resp[i+1].toString();
+					}
+					callback(err, data);
+				}
+			}
+		});
+	}
+	
+	//multi_hget name key1 key2 ... Get the values related to the specified multiple keys of a hashmap.
+	self.multi_hget = function(name, keys, callback){
+		if (name && keys && keys.length)
+			keys.unshift( name );
+				
+		self.request('multi_hget', keys, function(resp){
+	
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				var data = {};
+				for(var i=1; i<resp.length; i+=2){
+					data[ resp[i].toString() ] = resp[i+1].toString();
+				}
+				callback(err, data);
+			}
+		});
+	}
+	
+	//multi_hdel name key1 key2 ... Delete specified multiple keys in a hashmap.
+	self.multi_hdel = function(name, keys, callback){
+		if (name && keys && keys.length)
+			keys.unshift( name );
+				
+		self.request('multi_hdel', keys, function(resp){
+	
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];				
+				callback(err);
+			}
+		});
+	}
+	
+	//multi_hset name key1 value1 key2 value2 ... Set multiple key-value pairs(kvs) of a hashmap in one method call.
+	self.multi_hset = function(name, kv, callback){
+		let nkv = [];
+		if (name && kv){
+			nkv.push( name );
+			
+			Object.keys(kv).forEach(function(k){
+				if (kv[k]){
+					nkv.push( k );
+					nkv.push( kv[k] );
+				}
+			});
+		}
+				
+		self.request('multi_hset', nkv, function(resp){
+	
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];				
+				callback(err);
+			}
+		});
+	}
+	
+	
+	////////////////// Server command
+	//
+	
+	self.info = function(opt, callback){
+		if (!opt) opt = '';
+		self.request('info', [opt], function(resp){
+			if(callback){
+				var err = resp[0] == 'ok'? 0 : resp[0];
+				var data = [];
+				
+				for(var i=1; i<resp.length; i++){
+					var k = resp[i].toString();
+					data.push(k);
+				}
+				
+				callback(err, data);
 			}
 		});
 	}
